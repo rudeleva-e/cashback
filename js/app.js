820ff62dc24f3b66f1985e5f995378ec4fff0861
Student <Student@rosbank.ru>
2021-08-01 11:23:26 +0300

    calculator simple working version

--- js/app.js
+++ js/app.js
@@ -15,5 +15,42 @@ function calculateCashback(specialCategoryPurchases,otherCategoryPurchases) {
     };
 }
 
-const cashback=calculateCashback(5000,10000);
-console.log(cashback);
+
+function handleSubmit(evt) {
+    evt.preventDefault(); // отменяем поведение по умолчанию
+
+    // вытаскиваем значения, введенные пользователем и проверяем
+    const specialAmountInputEl = document.getElementById('special-amount-input');
+    const specialAmount = Number(specialAmountInputEl.value);
+    if (Number.isNaN(specialAmount)) {
+        // TODO: показать ошибку
+        return;
+    }
+    if (!Number.isFinite(specialAmount)) {
+        // TODO: показать ошибку
+        return;
+    }
+
+    const otherAmountInputEl = document.getElementById('other-amount-input');
+    const otherAmount = Number(otherAmountInputEl.value);
+    if (Number.isNaN(otherAmount)) {
+        // TODO: показать ошибку
+        return;
+    }
+    if (!Number.isFinite(otherAmount)) {
+        // TODO: показать ошибку
+        return;
+    }
+
+    // вызываем функцию и считаем кешбэк
+    const result = calculateCashback(specialAmount, otherAmount);
+    // находим элементы и добавляем к ним получившиеся значения
+    const specialCashbackEl = document.getElementById('special-cashback');
+    specialCashbackEl.textContent = `${result.specialCategoryCashback} руб.`;
+    const otherCashbackEl = document.getElementById('other-cashback');
+    otherCashbackEl.textContent = `${result.otherCategoryCashback} руб.`;
+    const totalCashbackEl = document.getElementById('total-cashback');
+    totalCashbackEl.textContent = `${result.totalCashback} руб.`;
+}
+const formEl = document.getElementById('cashback-form');
+formEl.onsubmit = handleSubmit;
